6faedb72f70ffe4e99e077749768299a8173ea35
Alex Shlemko <a.shlemko@lardansoft.com>
2019-12-19 00:13:13 +0200

    Файлы статики

---
+++ resources/assets/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="main">
+       <h1>vue`s working</h1>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        data: function () {
+            return {
+            }
+        },
+        methods: {
+            downloadFile() {
+                window.location += 'download';
+            },
+//            uploadFile(e) {
+//                var reader = new FileReader();
+//
+//                reader.readAsDataURL(e.target.files[0]);
+//
+//                reader.onload = (e) => {
+//                    this.file = e.target.result;
+//                }
+//            },
+
+        },
+        components:{
+        }
+    }
+
+</script>
+
+<style lang="scss">
+html{
+    font-family:Roboto sans-serif;
+}
+
+
+</style>    
---
+++ resources/assets/app.js
@@ -0,0 +1,24 @@
+
+
+
+import Vue from 'vue';
+import App from './App.vue'
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+const routes = [
+
+
+];
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+
+const app = new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+});
+global.app = app;
